ce3e7c154ef3e05f60238cfce8a9b36fce7504ac
DARKKNIGHT\Nahmaan <nahmaan@gmail.com>
2022-01-20 13:48:54 +0000

    Initial Service project created to return data from repository

---
+++ HelixService/Services/PatientService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HelixDataAccess.Interfaces;
+using HelixDataAccess.Models;
+using HelixDataAccess.Repositories;
+using HelixService.Interfaces;
+
+namespace HelixService.Services
+{
+    public class PatientService : IPatientService
+    {
+        private readonly IPatientRepository _patientRepository;
+
+        public PatientService(IPatientRepository patientRepository)
+        {
+            _patientRepository = patientRepository ?? new PatientRepository();
+        }
+        
+        public Patient GetAllPatients()
+        {
+            return _patientRepository.GetPatientData();
+        }
+    }
+}
